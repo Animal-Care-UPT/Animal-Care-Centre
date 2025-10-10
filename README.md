12763955f89f54e7e7c06b77a82f46a9a89984e1
Diogo Ferreira <192282837+diogof146@users.noreply.github.com>
2025-10-10 20:59:33 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# In Development
+This shall be updated at some point.
+
+## Team Members
+- Diogo Ferreira
+- Guilherme Santos
+- Sara Canelas
+- Tomás Falcão
---
+++ src/main/java/AnimalCareCentre/App.java
@@ -0,0 +1,14 @@
+package AnimalCareCentre;
+
+/**
+ * @author 51084 - Diogo Ferreira
+ * @author
+ * @author
+ * @author
+ */
+public class App {
+
+  public static void main(String[] args) {
+
+  }
+}
