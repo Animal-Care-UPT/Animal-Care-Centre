2ae936b2abb8de3f4b2e1cf60f2a2789d8758803
saracanelas <sara.i.canelas@gmail.com>
2025-10-18 21:04:45 +0100

    Created and updated Admin class (added attributes, constructor, getter and setters and toString)

---
+++ src/main/java/AnimalCareCentre/Admin.java
@@ -0,0 +1,15 @@
+package AnimalCareCentre;
+/**
+ * This class describes an Admin from the system, its attributes and what it can do.
+ *
+ */
+public class Admin extends Person {
+
+        //this class has no attributes of its own
+
+    //toString from the class
+    @Override
+    public String toString() {
+        return "Admin{" + super.toString() + "}";
+    }
+}
