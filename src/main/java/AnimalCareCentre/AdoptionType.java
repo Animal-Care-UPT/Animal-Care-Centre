704c34e51d3eecb74f9a9789048b93ecde2f7e1c
saracanelas <sara.i.canelas@gmail.com>
2025-10-18 20:24:20 +0100

    Created and updated AdoptionType class

---
+++ src/main/java/AnimalCareCentre/AdoptionType.java
@@ -0,0 +1,12 @@
+package AnimalCareCentre;
+/**
+ * This enum lists all the possible types of ways an animal can be rescued.
+ *
+ */
+public enum AdoptionType {
+
+    NOT_AVAILABLE,
+    FOR_ADOPTION,
+    FOR_FOSTER;
+
+}
