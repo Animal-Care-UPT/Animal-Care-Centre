3fee6a368d8e5a80485faae767218f7d19e22aa4
saracanelas <sara.i.canelas@gmail.com>
2025-10-18 21:11:14 +0100

    Added constructor to the class Admin

--- src/main/java/AnimalCareCentre/Admin.java
+++ src/main/java/AnimalCareCentre/Admin.java
@@ -1,4 +1,7 @@
 package AnimalCareCentre;
+
+import java.time.LocalDate;
+
 /**
  * This class describes an Admin from the system, its attributes and what it can do.
  *
@@ -6,6 +9,12 @@ package AnimalCareCentre;
 public class Admin extends Person {
 
         //this class has no attributes of its own
+
+
+    //Constructor from class
+    public Admin(String name, String email, String password, String location, SecurityQuestion securityQuestion, LocalDate birthDate) {
+        super(name, email, password, location, securityQuestion, birthDate);
+    }
 
     //toString from the class
     @Override
